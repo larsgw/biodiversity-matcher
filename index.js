5d439f24052648652d1c4790f8c509dd0d203501
Lars Willighagen <lars.willighagen@gmail.com>
2020-05-28 20:58:27 +0200

    feat: improve GUI, add attribution

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const guessData = {}
+const cache = {}
 
 const taxa = {
     Chelicerata: {
         Pycnogonida: 'Q19106',
         Xiphosura: 'Q19430',
         Arachnida: {
-            Scorpionida: 'Q30749508',
+            Scorpionida: 'Q19119', // (Pseudoscorpiones), other 'Q30749508'
             Opiliones: 'Q19116',
             Pseudoscorpionida: 'Q35135521',
-            Aranea: 'Q29024640',
+            Aranea: 'Q1357',
             Acari: {
                 Acariformes: 'Q1341457',
                 Parasitiformes: {
@@ -27,7 +28,7 @@ const taxa = {
     Crustacea: {
         Branchiopoda: {
             Anostraca: 'Q853383',
-            Cyclestherida: 'Q21217579',
+            Cyclestherida: 'Q768012', // (Diplostraca), other 'Q21217579'
             Notostraca: 'Q3344541',
             Cladocera: 'Q391240'
         },
@@ -90,45 +91,118 @@ function randomSample (array) {
     return array[Math.floor(Math.random() * array.length)]
 }
 
-function getSparql (leafTaxons) {
-    const taxon = randomSample(leafTaxons)
+function getTaxon (form) {
+    const taxa = Array.prototype.map.call(selection.taxon, input => input.value)
+    return randomSample(taxa)
+}
+
+function getSparql (taxon) {
     return `SELECT ?taxon ?taxonName ?taxonLabel ?species ?speciesLabel ?image WHERE {
-    BIND(wd:${taxon} AS ?taxon)
-    ?taxon wdt:P225 ?taxonName .
-    ?species wdt:P171+ ?taxon .
-    ?species wdt:P105 wd:Q7432 .
-    ?species wdt:P18 ?image .
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
-} LIMIT 10`
+        BIND(wd:${taxon} AS ?taxon)
+        ?taxon wdt:P225 ?taxonName .
+        ?species wdt:P171+ ?taxon .
+        ?species wdt:P105 wd:Q7432 .
+        ?species wdt:P18 ?image .
+        FILTER REGEX(STR(?image), "\.(jpg|jpeg|png|gif)$", "i")
+        SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+    } LIMIT 10`
+}
+
+async function getGuessData (taxon) {
+    if (!(taxon in cache) || cache[taxon].length === 0) {
+        const url = wdk.sparqlQuery(getSparql(taxon))
+        const json = await fetch(url).then(response => response.json())
+        const data = wdk.simplify.sparqlResults(json)
+        if (data.length === 0) {
+            console.log(taxon)
+        }
+        cache[taxon] = data
+    }
+
+    const guessData = randomSample(cache[taxon])
+    cache[taxon].splice(cache[taxon].indexOf(guessData), 1)
+
+    return guessData
+}
+
+async function getImageData (image) {
+    const filename = decodeURIComponent(image.replace(/^.+\//, ''))
+    const url = new URL('https://commons.wikimedia.org/w/api.php')
+    url.search = new URLSearchParams({
+        action: 'query',
+        titles: `File:${filename}`,
+        prop: 'imageinfo',
+        iiprop: 'url|extmetadata',
+        format: 'json',
+        origin: '*'
+    }).toString()
+    return fetch(url.href)
+        .then(response => response.json())
+        .then(response => Object.values(response.query.pages)[0].imageinfo[0])
+}
+
+function createCredits (data, element) {
+    if (data.extmetadata.Artist) {
+        element.innerHTML = data.extmetadata.Artist.value
+    }
+
+    element.appendChild(document.createTextNode(' via '))
+    const link = document.createElement('a')
+    link.href = data.descriptionurl
+    link.innerText = 'Wikimedia'
+    element.appendChild(link)
+    element.appendChild(document.createTextNode(' / '))
+
+    const license = document.createElement('a')
+    if (data.extmetadata.LicenseUrl) {
+        license.href = data.extmetadata.LicenseUrl.value
+    }
+    if (data.extmetadata.LicenseShortName) {
+        license.innerText = data.extmetadata.LicenseShortName.value
+    } else {
+        license.innerText = 'unknown'
+    }
+    element.appendChild(license)
 }
 
 selection.onsubmit = async function (e) {
     e.preventDefault()
     selection.disabled = true
-    const taxaSelection = Array.prototype.map.call(selection.taxon, input => input.value)
-    const url = wdk.sparqlQuery(getSparql(taxaSelection))
-    const json = await fetch(url).then(response => response.json())
-    const data = randomSample(wdk.simplify.sparqlResults(json))
+    guess.disabled = true
+    const taxon = getTaxon(selection)
+    guessData.data = await getGuessData(taxon)
+    guessData.imageData = await getImageData(guessData.data.image)
 
-    guessData.data = data
+    const figure = document.createElement('figure')
     const img = document.createElement('img')
-    img.src = data.image
+    img.src = guessData.data.image
+    figure.appendChild(img)
+    const caption = document.createElement('caption')
+    createCredits(guessData.imageData, caption)
+    figure.appendChild(caption)
+
     if (selection.images.firstChild) {
-        selection.images.removeChild(selection.images.firstChild);
+        selection.images.removeChild(selection.images.firstChild)
     }
-    selection.images.appendChild(img)
+    while (guess.result.firstChild) {
+        guess.result.removeChild(guess.result.lastChild)
+    }
+
+    selection.images.appendChild(figure)
 }
 
 guess.onsubmit = function (e) {
     e.preventDefault()
     const { taxon, species } = guessData.data
     if (taxon.value === guess.taxon.value) {
+        guess.result.dataset.success = true
         guess.result.innerHTML = `That is correct!
 This is a <i>${species.label}</i>, part of <i>${taxon.name}</i>`
         if (taxon.name !== taxon.label) {
             guess.result.innerHTML += ` (${taxon.label})`
         }
     } else {
+        guess.result.dataset.success = false
         guess.result.innerText = `Guess again.`
     }
 }
