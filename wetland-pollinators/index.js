dcf238e76bae506f2e4f57f084125110658d8264
Lars Willighagen <lars.willighagen@gmail.com>
2024-05-15 11:24:53 +0200

    feat(pollinators): switch wetland quiz to GBIF

--- wetland-pollinators/index.js
+++ wetland-pollinators/index.js
@@ -3,87 +3,87 @@ const cache = {}
 
 const data = {
     'Anthophila (bijen)': {
-        '<i>Andrena</i> (zandbijen)': 57669,
-        '<i>Apis mellifera</i> (honingbij)': 47219,
+        '<i>Andrena</i> (zandbijen)': 1345710,
+        '<i>Apis mellifera</i> (honingbij)': 1341976,
         '<i>Bombus</i> (hommels)': {
-            '<i>Bombus hortorum</i> (tuinhommel)': 121989,
-            '<i>Bombus humilis</i> (heidehommel)': 337776,
-            '<i>Bombus hypnorum</i> (boomhommel)': 61803,
-            '<i>Bombus jonellus</i> (veenhommel)': 452446,
-            '<i>Bombus lapidarius</i> (steenhommel)': 57619,
-            '<i>Bombus pascuorum</i> (akkerhommel)': 55637,
-            '<i>Bombus pratorum</i> (weidehommel)': 124910,
-            '<i>Bombus terrestris</i>-complex (aardhommels)': '57516,1092510',
-            '<i>Psithyrus</i> (koekoekshommels)': 538893,
-            other: 52775
+            '<i>Bombus hortorum</i> (tuinhommel)': 1340542,
+            '<i>Bombus humilis</i> (heidehommel)': 1340523,
+            '<i>Bombus hypnorum</i> (boomhommel)': 1340513,
+            '<i>Bombus jonellus</i> (veenhommel)': 1340308,
+            '<i>Bombus lapidarius</i> (steenhommel)': 1340301,
+            '<i>Bombus pascuorum</i> (akkerhommel)': 1340405,
+            '<i>Bombus pratorum</i> (weidehommel)': 1340305,
+            '<i>Bombus terrestris</i>-complex (aardhommels)': '1340503,1340298,5870368,4496017',
+            '<i>Psithyrus</i> (koekoekshommels)': '1340434,1340362,1340527,1340506,1340344,1340333,1340418',
+            other: 1340278
         },
-        '<i>Chelostoma</i> (klokjesbijen)': 357471,
-        '<i>Colletes</i> (zijdebijen)': 127741,
-        '<i>Dasypoda hirtipes</i> (pluimvoetbij)': 746682,
+        '<i>Chelostoma</i> (klokjesbijen)': 1334493,
+        '<i>Colletes</i> (zijdebijen)': 1348504,
+        '<i>Dasypoda hirtipes</i> (pluimvoetbij)': 1345858,
         '<i>Lasioglossum/Halictus</i> (groefbijen)': {
-            '<i>Halictus rubicundus</i> (roodpotige groefbij)': 127747,
-            '<i>Halictus scabiosae</i> (breedbandgroefbij)': 415589,
-            other: 335597
+            '<i>Halictus rubicundus</i> (roodpotige groefbij)': 1353453,
+            '<i>Halictus scabiosae</i> (breedbandgroefbij)': 1353395,
+            other: '1347978,1353501'
         },
-        '<i>Panurgus</i> (roetbijen)': 434201,
-        other: 630955
+        '<i>Panurgus</i> (roetbijen)': 1352891,
+        other: '7901,4334,7905,7908,7911,4345'
     },
     'Syrphidae (zweefvliegen)': {
-        '<i>Anasimyia/Eurimyia lineata</i>': 1144215,
+        '<i>Anasimyia/Eurimyia lineata</i>': 10366531,
         '<i>Cheilosia</i> (gitjes)': {
-            '<i>Cheilosia fraterna</i> (moerasgitje)': 155107,
-            other: 124483
+            '<i>Cheilosia fraterna</i> (moerasgitje)': 1540067,
+            other: 1539835
         },
         '<i>Chrysotoxum</i> (fopwespen)': {
-            '<i>Chrysotoxum bicinctum</i> (donkere fopwesp)': 337019,
-            other: 119997
+            '<i>Chrysotoxum bicinctum</i> (donkere fopwesp)': 1536009,
+            other: 1535870,
         },
-        '<i>Episyrphus balteatus</i> (snor-/marmelade-/pyjamazweefvlieg)': 52482,
-        '<i>Eristalinus</i> (vlekogen)': 145541,
+        '<i>Episyrphus balteatus</i> (snor-/marmelade-/pyjamazweefvlieg)': 1536449,
+        '<i>Eristalinus</i> (vlekogen)': 1542672,
         '<i>Eristalis</i> (bijvliegen)': {
-            '<i>Eristalis intricaria</i> (hommelbijvlieg)': 497713,
-            other: 52491
+            '<i>Eristalis intricaria</i> (hommelbijvlieg)': 1541262,
+            other: 1491190
         },
-        '<i>Eumerus</i> (bollenzweefvliegen)': 52233,
+        '<i>Eumerus</i> (bollenzweefvliegen)': 1534472,
         '<i>Eupeodes</i> (kommazwevers)': {
-            '<i>Eupeodes corollae</i> (terrasjeskommazwever)': 69190,
-            '<i>Eupeodes latifasciatus</i> (gele kommazwever)': 344449,
-            other: 62455,
+        '<i>Eupeodes corollae</i> (terrasjeskommazwever)': 1540742,
+        '<i>Eupeodes latifasciatus</i> (gele kommazwever)': 1540691,
+            other: 1540675
         },
         '<i>Helophilus</i> (pendelzweefvliegen)': {
-            '<i>Helophilus hybridus</i> (moeraspendelzweefvlieg)': 447126,
-            '<i>Helophilus pendulus</i> (gewone pendelzweefvlieg)': 52486,
-            '<i>Helophilus trivittatus</i> (citroenspendelzweefvlieg)': 70415
+            '<i>Helophilus hybridus</i> (moeraspendelzweefvlieg)': 5071368,
+            '<i>Helophilus pendulus</i> (gewone pendelzweefvlieg)': 5071352,
+            '<i>Helophilus trivittatus</i> (citroenspendelzweefvlieg)': 5071382
         },
         '<i>Melanostoma</i> (driehoekzweefvliegen)': {
-            '<i>Melanostoma mellinum</i> (gewone driehoekzweefvlieg)': 324344,
-            '<i>Melanostoma scalare</i> (slanke driehoekzweefvlieg)': 70405,
-            other: 70406
+            '<i>Melanostoma mellinum</i> (gewone driehoekzweefvlieg)': 5766554,
+            '<i>Melanostoma scalare</i> (slanke driehoekzweefvlieg)': 5766556,
+            other: 1533169
         },
-        '<i>Orthonevra</i> (glimmers)': 320037,
-        '<i>Platycheirus</i> (platvoetjes)': 121357,
+        '<i>Orthonevra</i> (glimmers)': 1537455,
+        '<i>Platycheirus</i> (platvoetjes)': 1542996,
         '<i>Pyrophaena</i>': {
-            '<i>Pyrophaena granditarsa</i> (klompvoetje)': 506529,
-            '<i>Pyrophaena rosarum</i> (vlinderstrikje)': 969881
+            '<i>Pyrophaena granditarsa</i> (klompvoetje)': 5886305,
+            '<i>Pyrophaena rosarum</i> (vlinderstrikje)': 4294542
         },
-        '<i>Rhingia campestris</i> (gewone snuitvlieg)': 355014,
+        '<i>Rhingia campestris</i> (gewone snuitvlieg)': 1537412,
         '<i>Scaeva</i> (halvemaanzwevers)': {
-            '<i>Scaeva pyrastri</i> (witte halvemaanzwever)': 52160,
-            '<i>Scaeva selenitica</i> (gele halvemaanzwever)': 349217
+        '<i>Scaeva pyrastri</i> (witte halvemaanzwever)': 1537719,
+        '<i>Scaeva selenitica</i> (gele halvemaanzwever)': 1537717
         },
         '<i>Sericomyia</i> (veenzweefvliegen)': {
-            '<i>Sericomyia lappona</i> (donkere veenzweefvlieg)': 486414,
-            '<i>Sericomyia silentis</i> (hoogveenzweefvlieg)': 70190,
+        '<i>Sericomyia lappona</i> (donkere veenzweefvlieg)': 1538831,
+        '<i>Sericomyia silentis</i> (hoogveenzweefvlieg)': 1538798
         },
-        '<i>Sphaerophoria</i> (langlijven)': 52965,
-        '<i>Syritta pipiens</i> (menuetzweefvlieg)': 81979,
-        '<i>Syrphus</i> (bandzweefvliegen)': 52489,
+        '<i>Sphaerophoria</i> (langlijven)': 1539106,
+        '<i>Syritta pipiens</i> (menuetzweefvlieg)': 1544431,
+        '<i>Syrphus</i> (bandzweefvliegen)': 1444606,
         '<i>Volucella</i> (reuzen)': {
-            '<i>Volucella bombylans</i> (hommelreus)': 1118722,
-            other: 52481
+            '<i>Volucella bombylans</i> (hommelreus)': 1541763,
+            other: 1483491
         },
-        '<i>Xylota/Chalcosyrphus</i> (bladlopers)': 488420,
-        other: 49995
+        '<i>Xylota/Chalcosyrphus</i> (bladlopers)': '1541512,1533584',
+        other: 6920
     }
 }
 
@@ -165,13 +165,18 @@ function getTaxon (form) {
     return randomSample(taxa)
 }
 
+function isExcluded (taxon, exclude) {
+    const parents = ['kingdomKey', 'phylumKey', 'classKey', 'orderKey', 'familyKey', 'genusKey', 'speciesKey']
+    return exclude.some(id => parents.some(parent => taxon[parent].toString() === id))
+}
+
 const selection = document.getElementById('selection')
 const taxaSelect = document.getElementById('taxaSelect')
 const guess = document.getElementById('guess')
 const giveup = document.getElementById('giveup')
 const taxaGuess = document.getElementById('taxaGuess')
 
-const licenses = ['cc-by', 'cc-by-nc', 'cc0', 'cc-by-sa', 'cc-by-nc-sa']
+const licenses = ['CC_BY_4_0', 'CC_BY_NC_4_0', 'CC0_1_0']
 
 function getTaxonIds (group) {
     if (typeof group === 'object') {
@@ -206,28 +211,35 @@ async function getGuessData (taxon) {
             }
         }
 
-        const url = new URL('https://api.inaturalist.org/v1/observations')
+        const url = new URL('https://api.gbif.org/v1/occurrence/search')
         const options = {
-            photos: true,
-            taxon_id: taxon,
-            place_id: 7506,
+            mediaType: 'StillImage',
+            taxonKey: taxon.split(','),
+            country: 'NL',
             license: licenses,
-            photo_license: licenses,
-            quality_grade: 'research',
-            locale: 'nl',
-            per_page: 25
+            // photo_license: licenses,
+            occurrenceStatus: 'PRESENT',
+            // locale: 'nl'
         }
 
-        if (exclude.length) {
-            options.without_taxon_id = exclude
+        const urlOptions = new URLSearchParams()
+        for (const key in options) {
+            for (const value of [].concat(options[key])) {
+                urlOptions.append(key, value)
+            }
         }
-        url.search = new URLSearchParams(options).toString()
+
+        url.search = urlOptions.toString()
 
         const data = await fetch(url).then(response => response.json())
-        if (data.results == null || data.results.length === 0) {
+
+        let results = data.results ?? []
+        results = results.filter(result => !isExcluded(result, exclude))
+
+        if (results.length === 0) {
             return undefined
         }
-        cache[key] = data.results
+        cache[key] = results
     }
 
     const guessData = randomSample(cache[key])
@@ -268,23 +280,26 @@ selection.onsubmit = async function (e) {
           value: taxon
         },
         species: {
-          name: data.taxon.name,
-          label: data.taxon.preferred_common_name
+          name: data[data.taxonRank.toLowerCase()]
         }
     }
 
     // Update images
     const figure = document.createElement('figure')
     const container = document.createElement('div')
-    for (const photo of data.photos) {
+    for (const photo of data.media) {
+        if (photo.type !== 'StillImage') {
+            continue
+        }
+
         const figure = document.createElement('figure')
 
         const img = document.createElement('img')
-        img.src = photo.url.replace('square', 'large')
+        img.src = photo.identifier.replace('original', 'large')
         figure.appendChild(img)
 
         const figcaption = document.createElement('figcaption')
-        figcaption.innerText = photo.attribution
+        figcaption.innerText = `© ${photo.rightsHolder} (${photo.license})`
         figure.appendChild(figcaption)
 
         container.appendChild(figure)
@@ -292,7 +307,7 @@ selection.onsubmit = async function (e) {
     figure.appendChild(container)
     const figcaption = document.createElement('figcaption')
     const a = document.createElement('a')
-    a.href = data.uri
+    a.href = `https://www.gbif.org/occurrence/${data.identifier}`
     a.innerText = 'Observation link (spoilers)'
     figcaption.appendChild(a)
     figure.appendChild(figcaption)
