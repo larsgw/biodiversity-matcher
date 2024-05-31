50b1076539707449bb8f98869e60896e9c3da015
Lars Willighagen <lars.willighagen@gmail.com>
2024-05-31 14:02:31 +0200

    feat(data): add invasive-mollusca quiz

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
+import invasiveMollusca from './invasive-mollusca.js'
 import pollinators from './pollinators.js'
 import wetlandPollinators from './wetland-pollinators.js'
 
 export default {
+  'invasive-mollusca': invasiveMollusca,
   'pollinators': pollinators,
   'wetland-pollinators': wetlandPollinators
 }
